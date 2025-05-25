9b2a4c208f6ba8c6d1c4cfc3206a6fc2700ea769
ArnyBalbin <arnybalbin2005@gmail.com>
2025-05-25 14:05:15 -0500

    subido1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,10 +8,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
   }
-})
+});
